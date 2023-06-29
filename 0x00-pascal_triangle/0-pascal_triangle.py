16d9e491a7ebc5fb948744a5e229896785fc8e22
H3c7o4 <hectorvladitok@gmail.com>
2023-06-29 16:32:14 +0200

    Fix some bugs

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,29 @@
-#!/usr/bin/env python3
-"""pascal_triangle module.
-
-Contains a function that returns a list of lists of
-integers representing the Pascal’s triangle of n.
+#!/usr/bin/python3
+"""
+0-pascal_triangle
 """
 
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers
-    representing the Pascal’s triangle of n.
+    :type n: int
+    :return type: int
     """
+    mat = list(range(n))
+    mat[0] = [1]
+    mat[1] = [1, 1]
+
     if n <= 0:
         return []
-
-    result = [[1]]
-    while len(result) is not n:
-        tmp = [1]
-        for i in range(len(result[-1]) - 1):
-            tmp.append(result[-1][i] + result[-1][i + 1])
-        tmp.append(1)
-        result.append(tmp)
-    return result
+    if n == 1:
+        return mat[0]
+    if n == 2:
+        return mat
+    if n > 2:
+        for i in range(2, n):
+            mat[i] = [
+                mat[i - 1][j] + mat[i - 1][j + 1]
+                for j in range(len(mat[i - 1]) - 1)
+            ]
+            mat[i] = [1] + mat[i] + [1]
+        return mat
